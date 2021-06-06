24f4e62749c237cba7174e753c57370cdd219e44
gamzecomertp <gamzecomert8@gmail.com>
2021-06-06 10:57:36 +0200

    city updated

--- src/javas.js
+++ src/javas.js
@@ -18,7 +18,7 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let currentDay = days[date.getDay()];
-  return `Last updated at: ${currentDay} ${currentHour}:${currentMinutes}`;
+  return `Last updated: ${currentDay} ${currentHour}:${currentMinutes}`;
 }
 
 function displayTemperature(response) {
@@ -41,6 +41,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "66af7215126c3001af4a20c9cde9c1c1";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}&units=metric`;
+let city = "Warsaw";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
