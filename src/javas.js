46b0c499188ad2d962b5cd3ca6cba32c2b666a29
gamzecomertp <gamzecomert8@gmail.com>
2021-06-06 10:18:47 +0200

    java updated

--- src/javas.js
+++ src/javas.js
@@ -1,3 +1,25 @@
+function formatDate(date) {
+  let currentHour = date.getHours();
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+  let currentMinutes = date.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[date.getDay()];
+  return `Last updated at: ${currentDay} ${currentHour}:${currentMinutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature1");
   document.querySelector("#city").innerHTML = response.data.name;
@@ -11,6 +33,7 @@ function displayTemperature(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  document.querySelector("date").innerHTML = response.data.dt * 1000;
 }
 
 let apiKey = "66af7215126c3001af4a20c9cde9c1c1";
