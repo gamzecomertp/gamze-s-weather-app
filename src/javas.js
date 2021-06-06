8d885f10171e074dbb52293b14c4a2614ee6b577
gamzecomertp <gamzecomert8@gmail.com>
2021-06-06 10:19:37 +0200

    java

--- src/javas.js
+++ src/javas.js
@@ -39,4 +39,4 @@ function displayTemperature(response) {
 let apiKey = "66af7215126c3001af4a20c9cde9c1c1";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=${apiKey}&units=metric`;
 
-axios.get(apiURL).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
